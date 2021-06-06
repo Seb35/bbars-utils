b21fe72b946b62e978fee7c501ce7d8f627334e4
Sébastien Beyou <sebastien.beyou@wiki-valley.com>
2021-06-06 13:11:12 +0200

    Add Base64hex and Base16hex.
    This base64hex is preserving bitwise sort order contrary to
    classical base64.
    
    The Base16hex is nothing special, it is identical to bin2hex,
    but it is used here for performance comparison with Base32hex
    and Base64hex.

--- php-base32-encode-decode/Base32.php
+++ php-base32-encode-decode/Base32.php
@@ -79,3 +79,153 @@ class Base32hex extends Base32
 {
 	const CHARS = '0123456789abcdefghijklmnopqrstuv'; // lower-case
 }
+
+class Base64hex
+{
+	const BITS_6_RIGHT = 63;
+	const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz{}';
+
+	public static function encode($data, $padRight = false)
+	{
+		$dataSize = strlen($data);
+		$res = '';
+		$remainder = 0;
+		$remainderSize = 0;
+
+		for ($i = 0; $i < $dataSize; $i++)
+		{
+			$b = ord($data[$i]);
+			$remainder = ($remainder << 8) | $b;
+			$remainderSize += 8;
+			while ($remainderSize > 5)
+			{
+				$remainderSize -= 6;
+				$c = $remainder & (self::BITS_6_RIGHT << $remainderSize);
+				$c >>= $remainderSize;
+				$res .= static::CHARS[$c];
+			}
+		}
+		if ($remainderSize > 0)
+		{
+			// remainderSize < 6:
+			$remainder <<= (6 - $remainderSize);
+			$c = $remainder & self::BITS_6_RIGHT;
+			$res .= static::CHARS[$c];
+		}
+		if ($padRight)
+		{
+			$padSize = (8 - ceil(($dataSize % 6) * 8 / 6)) % 8;
+			$res .= str_repeat('=', $padSize);
+		}
+
+		return $res;
+	}
+
+	public static function decode($data)
+	{
+		$data = rtrim($data, "=\x20\t\n\r\0\x0B");
+		$dataSize = strlen($data);
+		$buf = 0;
+		$bufSize = 0;
+		$res = '';
+		$charMap = array_flip(str_split(static::CHARS)); // char=>value map
+		$charMap += array_flip(str_split(strtoupper(static::CHARS))); // add upper-case alternatives
+
+		for ($i = 0; $i < $dataSize; $i++)
+		{
+			$c = $data[$i];
+			if (!isset($charMap[$c]))
+			{
+				if ($c == " " || $c == "\r" || $c == "\n" || $c == "\t")
+					continue; // ignore these safe characters
+				throw new Exception('Encoded string contains unexpected char #'.ord($c)." at offset $i (using improper alphabet?)");
+			}
+			$b = $charMap[$c];
+			$buf = ($buf << 6) | $b;
+			$bufSize += 6;
+			if ($bufSize > 7)
+			{
+				$bufSize -= 8;
+				$b = ($buf & (0xff << $bufSize)) >> $bufSize;
+				$res .= chr($b);
+			}
+		}
+
+		return $res;
+	}
+}
+
+class Base16hex
+{
+	const BITS_4_RIGHT = 15;
+	const CHARS = '0123456789abcdef'; // lower-case
+
+	public static function encode($data, $padRight = false)
+	{
+		$dataSize = strlen($data);
+		$res = '';
+		$remainder = 0;
+		$remainderSize = 0;
+
+		for ($i = 0; $i < $dataSize; $i++)
+		{
+			$b = ord($data[$i]);
+			$remainder = ($remainder << 8) | $b;
+			$remainderSize += 8;
+			while ($remainderSize > 3)
+			{
+				$remainderSize -= 4;
+				$c = $remainder & (self::BITS_4_RIGHT << $remainderSize);
+				$c >>= $remainderSize;
+				$res .= static::CHARS[$c];
+			}
+		}
+		if ($remainderSize > 0)
+		{
+			// remainderSize < 6:
+			$remainder <<= (4 - $remainderSize);
+			$c = $remainder & self::BITS_4_RIGHT;
+			$res .= static::CHARS[$c];
+		}
+		if ($padRight)
+		{
+			$padSize = (8 - ceil(($dataSize % 4) * 8 / 4)) % 8;
+			$res .= str_repeat('=', $padSize);
+		}
+
+		return $res;
+	}
+
+	public static function decode($data)
+	{
+		$data = rtrim($data, "=\x20\t\n\r\0\x0B");
+		$dataSize = strlen($data);
+		$buf = 0;
+		$bufSize = 0;
+		$res = '';
+		$charMap = array_flip(str_split(static::CHARS)); // char=>value map
+		$charMap += array_flip(str_split(strtoupper(static::CHARS))); // add upper-case alternatives
+
+		for ($i = 0; $i < $dataSize; $i++)
+		{
+			$c = $data[$i];
+			if (!isset($charMap[$c]))
+			{
+				if ($c == " " || $c == "\r" || $c == "\n" || $c == "\t")
+					continue; // ignore these safe characters
+				throw new Exception('Encoded string contains unexpected char #'.ord($c)." at offset $i (using improper alphabet?)");
+			}
+			$b = $charMap[$c];
+			$buf = ($buf << 4) | $b;
+			$bufSize += 4;
+			if ($bufSize > 7)
+			{
+				$bufSize -= 8;
+				$b = ($buf & (0xff << $bufSize)) >> $bufSize;
+				$res .= chr($b);
+			}
+		}
+
+		return $res;
+	}
+}
